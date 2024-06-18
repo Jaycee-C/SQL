38803de1219fa8ec3cf10279048edae740bbae9d
Jaea Cho <jaeacho@Jaeas-MacBook-Pro.local>
2024-06-18 15:49:30 -0400

    moving files into SQL50 directory

--- 1661.Average Time of Process per Machine.sql
+++
@@ -1,6 +0,0 @@
-SELECT e.machine_id, ROUND(AVG(ABS(e.timestamp - s.timestamp)),3) AS processing_time
-FROM Activity e
-JOIN Activity s ON e.machine_id = s.machine_id AND e.process_id = s.process_id
-WHERE e.activity_type = "end" AND s.activity_type ="start"
-GROUP BY e.machine_id;
-
